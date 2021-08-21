b813d39739cb87bbf94b01ce257307dda2893deb
work-leonid <work.leonid@gmail.com>
2021-08-21 14:09:57 +0300

    feat: add vercel json

---
+++ dist/_nuxt/static/1629543754/ru/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629543754",layout:"sidebar",error:a,serverRendered:true,routePath:"\u002Fru\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
